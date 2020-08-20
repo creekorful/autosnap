6789e907708bacf0227e3f8e3aeea0f9d138229c
Aloïs Micard <lunamicard@gmail.com>
2020-08-20 22:44:04 +0200

    Add clap-rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{App, AppSettings, Arg, crate_authors, crate_version};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("autosnap")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Automatically make Snap package from github repository")
+        .arg(
+            Arg::with_name("src")
+                .value_name("SRC")
+                .required(true)
+                .help("The github repository (example: github.com/creekorful/osync)."),
+        )
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .get_matches();
+
+    let src = matches.value_of("src").unwrap();
+    print!("{}", src);
 }
